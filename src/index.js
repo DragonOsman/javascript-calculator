ad525b9e8d19127d6a56ee454baeb1636366c47c
Osman Zakir <osmanzakir90@hotmail.com>
2020-09-22 02:56:40 +0500

    moved css file and updated sass script command

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./js/components/App";
-import "./style.css";
+import "./styles/style.css";
 
 // Opt-in to Webpack hot module replacement
 if (module.hot) {
