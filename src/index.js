932fcaeeea8b4e7b6abd6300c495a396e4c44f6c
Osman Zakir <osmanzakir90@hotmail.com>
2020-09-22 02:42:27 +0500

    added index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./js/components/App";
+import "../css/style.css";
+
+// Opt-in to Webpack hot module replacement
+if (module.hot) {
+  module.hot.accept();
+}
+
+const rootDiv = document.getElementById("root");
+ReactDOM.render(<App />, rootDiv);
