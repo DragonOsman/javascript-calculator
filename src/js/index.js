abfa999911fb80dfb43549e4429604bfa718ec54
Osman Zakir <osmanzakir90@hotmail.com>
2020-09-27 19:17:44 +0500

    moved index.js to /js folder and updated CSS import statement

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./js/components/App";
-import "./styles/style.css";
+import App from "./components/App";
+import "../styles/style.css";
 
 const rootDiv = document.getElementById("root");
 ReactDOM.render(<App />, rootDiv);
